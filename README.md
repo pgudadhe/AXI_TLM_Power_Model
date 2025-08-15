4105dd1f1e6ebbbb16affc6f10908e7320ca10df
Pradnyesh Gudadhe <CodeDharma@outlook.com>
2025-08-14 20:35:54 -0700

    Power API added

--- README.md
+++ README.md
@@ -8,3 +8,60 @@ git clone https://github.com/UoS-EEC/fused
 ```
 
 Follow the instruction to build the library. We will be linking this library in our AXI project.
+I will also be using Power Logger wrapper created by zahrajnb in my code. This wrapper class provided useful utilities to simplify the use of fused library.
+
+In this project we are going to imagine a scenario of a AXI Master and AXI Target module connected by AMBA AXI4 bus. The master could be a hardware accelerator creating a series of Read/Write demands to the target which could be the host memory. I assume that behind the development of this performance model, the intention of the Architect is to find out what would the most optimal bandwidth for Read and Write bus of the AXI data channel. AXI bus consists of multiple channels address, data, response channels for both read and write operations as shown below:
+
+
+<img width="1364" height="529" alt="image" src="https://github.com/user-attachments/assets/ad957df3-93fc-4580-a45c-ef7fd15378d8" />
+
+
+Further details can be found at AXI spec which tell about specific signals contained in these buses. For example, Write Address Channel contains awburst, awaddr, awlen, awsize, awready, awvalid signals and buses. But we are not aiming to model a Pin Accurate model of this scenario like an RTL. Our aim is to build an Approximately Timed (AT) Transaction Level Model (TLM) using SystemC for this scenario in order to observe Read/Write throughput in this scanrio by tweaking crucial knobs for this design as we will look ahead in details.
+
+## Building the model:
+
+This project utilizes CMake inorder to build. I assume that your enviornment has been setup using SystemC+WSL+VSCode set up that I have explained in previous post [here](https://pgudadhe.github.io/).
+Follow the steps in these articles to build the model.
+
+## Configurations:
+
+This model provides various knobs to configure various scenarios.
+
+```bash
+Usage: ./AXI_TLM_PERF_MODEL [options]
+Options:
+  -h                           Show this help message
+  -n <no. of reqs>             Specify number of R/W requests
+  -s <request size>            Specify request size in bytes (default 0: 4B to 4096B randomized size), else: Absolute fixed size
+  -r <Wr to Rd ratio>          Specify the ratio of Write requests vs. Read. eg. 0.7: 70% Write reqs, 30% Read reqs.
+  -d <debug level>             Specify debug level (0-3)
+  -rb <AXI Read Bus Width>     Read bus width. 0:4B, 1:8B, 2:16B 3:32B, 4:64B
+  -wb <AXI Read Bus Width>     Write bus width. 0:4B, 1:8B, 2:16B 3:32B, 4:64B
+Example: ./AXI_TLM_PERF_MODEL -n 10 -r 0.5 -rb 2 -wb 3
+This will run the simulation with 10 requests, 50% Write & 50% Read requests,
+with Read bus width of 16B and Write bus width of 32B.
+Default values: -n 10, -r 0.5, -rb 0, -wb 0
+```
+
+| Commandline Option      | Description |
+| ----------- | ----------- |
+| n | Users can specify the number of requests that need to be made to the AXI target |
+| s | Users can keep a fixed size of all requests (upto 4KB) or it can be randomized |
+| r | No. of Read requests vs Write requests can be randomized based on probability distribution of Bernoulli |
+| rb/wr | Key knobs of this model where user can vary the bandwidth of the Read and Write data buses |
+
+Running the model with default options would show an output like this:
+
+```bash
+Info: /OSCI/SystemC: Simulation stopped by user.
+Total Read Delay: 32730 ns 
+Total Write Delay: 20750 ns
+```
+
+## Power Data Analysis:
+I ran multiple experiments on this model collecting data by varying these knobs individually or together. Based on the data I collected, this is the performance analysis I observed. 
+
+References:
+
+[https://github.com/UoS-EEC/fused](https://github.com/UoS-EEC/fused)
+[https://github.com/zahrajnb/DomainPowerModeling](https://github.com/zahrajnb/DomainPowerModeling)
---
+++ power_domain_logger.cpp
@@ -0,0 +1,133 @@
+/**
+    @file power_domain_logger.cpp
+    @brief Class to log power numbers for each domain
+    @version 1.0.0 
+    @date 30/06/23 
+*/
+#include "power_domain_logger.h"
+
+using namespace sc_core;
+using namespace sc_dt;
+using namespace std;
+
+SC_HAS_PROCESS(power_domain_logger);
+
+power_domain_logger::power_domain_logger(const sc_module_name name,
+                        double domainVoltage,
+                        const std::string logFilePath,
+                        sc_time logTimestep): 
+        sc_module(name),
+        domainVoltage(domainVoltage),
+        m_power_domain_current_LogFileName(logFilePath == "none"
+                             ? "none"
+                             : logFilePath + "/"  +
+                                   "currentlog.csv"),                       
+        m_logTimestep(logTimestep)
+{
+    if (logFilePath != "none") {
+        // Make directory if it doesn't exist
+        std::string makedir = "mkdir -p " + logFilePath;
+        spdlog::info(logFilePath);
+        system(makedir.c_str());
+        // Create/overwrite log files
+        std::ofstream log_file(m_power_domain_current_LogFileName, std::ios::out | std::ios::trunc);
+        if (!log_file.good()) {
+            SC_REPORT_FATAL(
+            this->name(),
+            fmt::format("Can't open current log file at {}", m_power_domain_current_LogFileName).c_str());
+        } 
+    }
+    SC_HAS_PROCESS(power_domain_logger);
+    SC_THREAD(log_process);
+
+}
+
+void power_domain_logger::power_connector (string moduleID, string moduleType, PowerModelEventOutPort* powerPort, string powerDomain){
+
+    string PowerChannel_name_str = moduleID + "PowerChannel";
+    string PowerBridge_name_str = moduleID + "PowerBridge";
+    string Report_location = "../reports/" + powerDomain + "/" + moduleType + "/" + moduleID;
+    
+    power_channels.push_back(new PowerModelChannel(PowerChannel_name_str.c_str(), Report_location, m_logTimestep));
+    power_bridges.push_back(new PowerModelBridge(PowerBridge_name_str.c_str(), m_logTimestep));
+
+    PowerModelChannel *module_p_channel = power_channels.back();
+    PowerModelBridge *module_p_bridge = power_bridges.back();
+    
+    sc_signal<double>* current_bind = new sc_signal<double>();
+    voltage = domainVoltage;
+    module_p_bridge->v_in.bind(voltage);
+    module_p_bridge->i_out.bind(*current_bind);
+
+    // Creating a new struct to save the current value and the module ID in the vector
+    CurrentModule* currentModule = new CurrentModule;
+    currentModule->module_current = current_bind; // Read the initial value of the signal
+    currentModule->moduleID = moduleID;
+    currentModule->powerDomain = powerDomain;
+    currents.push_back(currentModule);
+
+    // If the power domain is not yet in the map, initialize its total current to 0.0
+    if (total_current_per_domain.find(powerDomain) == total_current_per_domain.end()) {
+        total_current_per_domain[powerDomain] = 0.0;
+    }
+
+    module_p_bridge->powerModelPort.bind(*(module_p_channel));
+    (*powerPort).bind(*(module_p_channel));  
+}
+
+void power_domain_logger::log_process() {
+    
+    std::vector<std::string> savedModuleIDs;  // Vector to store previously saved moduleIDs
+
+    while (1) {
+        wait(m_logTimestep);
+        // Go through my whole vector elements check the module_current value the first time when gets a velue other than 0 and add that value to the sum only once per moduleID
+        // Iterate over the currents vector
+        for (const auto& currentModule : currents) {
+            double currentVal = currentModule->module_current->read();
+            std::string currentModuleID = currentModule->moduleID;
+            std::string powerDomain = currentModule->powerDomain; 
+            spdlog::info("current for module {} in {} is {}", currentModuleID, powerDomain, currentVal);
+
+            // Check if the moduleID is already saved
+            if (currentVal != 0.0 && std::find(savedModuleIDs.begin(), savedModuleIDs.end(), currentModuleID) == savedModuleIDs.end()) {
+                // Adds all the values of current from modules in each domain calling the power_connector
+                total_current_per_domain[powerDomain] += currentVal;
+                savedModuleIDs.push_back(currentModuleID);
+                spdlog::info("accumulated current for domain {}: {}", powerDomain, total_current_per_domain[powerDomain]);
+                
+            }
+        }
+        // Call dumpCurrentCsv to write the vector to the CSV file
+        dumpCurrentCsv();
+    }
+}
+
+void power_domain_logger::dumpCurrentCsv()
+{
+    temp_current = 0.0;
+    std::ofstream log_file(m_power_domain_current_LogFileName, std::ios::out | std::ios::app);
+    // Write elements of the currents vector to the CSV file
+    if (log_file.tellp() == 0) {
+        // Header
+        log_file << "moduleID,module_current,time(s)\n";
+    }
+    // Values
+    for (const auto& module : currents) {
+        log_file << module->moduleID << ",";
+        log_file << module->module_current->read() << ",";
+        log_file << sc_time_stamp().to_seconds() << "\n";
+    }
+    log_file.close();  // Close the file
+}
+
+power_domain_logger::~power_domain_logger()
+{
+    // deleting the pointers 
+    for (PowerModelChannel* channel_ptr : power_channels) {
+        delete channel_ptr;
+    }
+    for (PowerModelBridge* bridge_ptr : power_bridges) {
+        delete bridge_ptr;
+    }
+}
